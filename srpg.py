db085699d072c27b8da22c722936b1bc94637fa8
Aaron Scherer <aequasi@gmail.com>
2014-04-25 18:28:53 -0700

    Removing myql

--- srpg.py
+++ srpg.py
@@ -2,28 +2,33 @@ from srpgCore import srpgCore
 import es
 
 import psyco
-psyco.full( )
+
+psyco.full()
 
 core = srpgCore()
 info = core.info
 
-def load( ):
-	""" Runs when the addon is loaded. Loads all necessary files into memory """
-	print core
-	core.load( )
 
-def unload( ):
-	core.unload()
+def load():
+    """ Runs when the addon is loaded. Loads all necessary files into memory """
+    print core
+    core.load()
+
+
+def unload():
+    core.unload()
+
 
 def cmdHandler():
-	# Get command name
-	name = es.getargv(1)
-	print "SRPG Called - %s" % name
-	core.command.run( es.getcmduserid(), name, es.getargs() )
+    # Get command name
+    name = es.getargv(1)
+    print "SRPG Called - %s" % name
+    core.command.run(es.getcmduserid(), name, es.getargs())
 
 
-def player_connect( event ):
-	core.events[ 'main' ].event_player_connect( event )
+def player_connect(event):
+    core.events['main'].event_player_connect(event)
 
-def player_death( event ):
-	core.events[ 'main' ].event_player_death( event )
+
+def player_death(event):
+    core.events['main'].event_player_death(event)
